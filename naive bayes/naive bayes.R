6884beea819c5900cdd47d7c8b2cb0dd24be44a0
Clinton Moshe <clintonmwachia9@gmail.com>
2021-05-27 22:51:45 +0300

    naive bayes

--- naive bayes/naive bayes.R
+++ naive bayes/naive bayes.R
@@ -1,11 +1,9 @@
-# load the libraries
 library(mlbench)
 library(caret)
 library(e1071)
 library(naivebayes)
-library(DataExplorer)
+library(DataExplorer) # IF NOT INSTALLED, RSTUDIO WILL DETECT AND ASK IF IT SHOULD INSTALL
 
-# load the data
 data("PimaIndiansDiabetes")
 
 data <- PimaIndiansDiabetes
@@ -16,74 +14,70 @@ str(data)
 
 colSums(is.na(data))
 
-# visualize na values
 plot_missing(data)
-# exploring the data
+
 tb <- table(data$diabetes)
 tb
 
 prop.table(tb)*100
 
-# imbalanced data set
 # random down sampling
 index_neg <- sample(1:400, 268)
 
-# creating the seperate classes
 neg <- data[which(data$diabetes == "neg"),]
+
 pos <- data[which(data$diabetes == "pos"),]
 
 neg_data <- neg[index_neg,]
 
-undersample_data <- rbind(pos,neg_data)
+undersample_data <- rbind(pos, neg_data)
 
 table(undersample_data$diabetes)
-
 # random up sampling
-index_pos1 <- sample(1:232,232)
+index_pos1 <- sample(1:232, 232)
 
 pos_data <- pos[index_pos1,]
 
-pos_data <- rbind(pos_data, pos)
+pos_data <- rbind(neg, pos_data)
 
-upsample_data <- rbind(pos_data, neg)
+upsample_data <- rbind(pos_data, neg_data)
 
-table(upsample_data$diabetes)
-
-# split the data
-index <- createDataPartition(upsample_data$diabetes, list = F,p = 0.8)
+#
+index <- createDataPartition(upsample_data$diabetes, list = F, p= 0.8)
 
 train <- upsample_data[index,]
+
 test <- upsample_data[-index,]
 
 # the model
-model <- naiveBayes(diabetes~., data = train)
+model <- naiveBayes(diabetes ~., data = train)
 model
 
-# predictions
-pred <- predict(model, newdata = test)
-
-tb <- table(pred, test$diabetes)
-tb
-
-confusionMatrix(tb)
-
-# method
-trcontrol <- trainControl(
-  method = "cv",
-  number = 10
-) 
-
-model1 <- train(
-  diabetes~.,
-  data = train,
-  method ="naive_bayes",
-  preProcess = c("scale", "center")
-)
-
-# predictions
-pred1 <- predict(model1, newdata = test, type = "raw")
+pred1 <- predict(model, newdata = test)
 
 tb1 <- table(pred1, test$diabetes)
 tb1
 
 confusionMatrix(tb1)
+
+# the percentage changes every time you run the model
+
+# method 2
+trcontrol <- trainControl(
+  method = "cv",
+  number = 10
+)
+
+model1 <- train(
+  diabetes ~., 
+  data = train,
+  method = "naive_bayes",
+  preProcess = c("scale","center")
+)
+
+pred1 <- predict(model1, test, "raw")
+
+tb <- table(pred1, test$diabetes)
+tb
+
+confusionMatrix(tb)
